06a58b4975ddc4013d66b92ae134f394108ed11f
Vitor Vaz <vitor.vaz.andrade@gmail.com>
2021-05-16 22:26:51 -0300

    removendo zumbi code  #4

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -88,36 +88,4 @@ module.exports = {
         }
     },
 
-    async create(req, res) {
-        const { body } = req;
-        try {
-            UserModel.postUser(body);
-            res.send({ mensagem: 'usuario inserido com sucesso' });
-        } catch (error) {
-            res.status(400).send({ error: 'usuario não cadastrado' });
-        }
-    },
-
-    async deleteById(req, res) {
-        const { id } = req.params;
-        try {
-            UserModel.delete(id);
-            res.send({ mensagem: 'usuário deletado com sucesso' });
-        } catch (error) {
-            res.send(error);
-        }
-    },
-
-    async putById(req, res) {
-        const { id } = req.params;
-        const { body } = req;
-
-        try {
-            await UserModel.updateUser(body, id);
-            res.send({ mensagem: 'usuário atualizado com sucesso' });
-        } catch (error) {
-            res.send(error);
-        }
-    },
-
 };
