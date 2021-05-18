03c2aa301f5380a06917d648509176390e9fab4b
Vitor Vaz <vitor.vaz.andrade@gmail.com>
2021-05-17 22:09:18 -0300

    primeira implantação de validações de campos #6

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,84 +1,160 @@
 const User = require('../models/user');
+const { body, validationResult } = require('express-validator');
 
 module.exports = {
 
-  async getAll(req, res) {
-    const users = await User.findAll();
+    async getAll(req, res) {
 
-    res.send(users);
-  },
+        const users = await User.findAll();
 
-  async getById(req, res) {
-    const { id } = req.params;
 
-    const user = await User.findOne({
-      where: {
-        id,
-      },
-    });
-    res.send(user);
-  },
+        res.send(users);
+    },
 
-  async create(req, res) {
-    const {
-      name,
-      avatar,
-      login,
-      password,
-      email,
-    } = req.body;
 
-    const user = await User.create({
-      name, avatar, email, login, password, coins: 1000,
-    });
+    async getById(req, res) {
 
-    res.send(user);
-  },
+        const id = req.params.id
 
-  async deleteById(req, res) {
-    const { id } = req.params;
-    try {
-      const deleted = await User.findOne({
-        where: {
-          id,
-        },
-      });
+        try{
+            const user = await User.findByPk(id);
 
-      deleted.destroy();
-      res.send({ mensagem: 'usuário deletado com sucesso' });
-    } catch (error) {
-      res.send(error);
+            if(!user){
+                throw new Error(`não foi encontrado o usuario com o id: ${id}`)
+            }
+            res.send(user);
+        }catch(error){
+            res.send({error: error.message})
+        }
+
+    },
+
+
+    create: {
+
+        validating: [
+            body('name').notEmpty().withMessage("O preenchimento desse campo é obrigatório"),
+            body('name').isString().withMessage("Esse campo não aceita numeros"),
+            body('avatar').notEmpty().withMessage("O preenchimento desse campo é obrigatório"),
+            body('avatar').isString().withMessage("Esse campo não aceita numeros"),
+            body('login').notEmpty().withMessage("O preenchimento desse campo é obrigatório"),
+            body('login').isString().withMessage("Esse campo não aceita numeros"),
+            body('password').notEmpty().withMessage("O preenchimento desse campo é obrigatório"),
+            body('password').isString().withMessage("Esse campo não aceita numeros"),
+            body('email').notEmpty().withMessage("O preenchimento desse campo é obrigatório"),
+            body('email').isString().withMessage("Esse campo não aceita numeros"),
+
+        ],
+
+        creating: async (req, res) => {
+
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+
+                return res.status(400).json({ errors: errors.array() });
+            } else {
+
+                const {
+                    name,
+                    avatar,
+                    login,
+                    password,
+                    email
+                } = req.body;
+
+                const user = await User.create({ name, avatar, email, login, password, coins: 1000 });
+
+                res.send(user);
+            }
+        }
+    },
+
+    async deleteById(req, res) {
+        const id = req.params.id
+        try {
+
+            const deleted = await User.findOne({
+                where: {
+                    id: id
+                }
+            })
+
+            deleted.destroy();
+            res.send({ mensagem: 'usuário deletado com sucesso' })
+        } catch (error) {
+            res.send(error)
+        }
+
+    },
+
+
+    update: {
+        validating: [
+            body('name').notEmpty().withMessage("O preenchimento desse campo é obrigatório"),
+            body('name').isString().withMessage("Esse campo não aceita numeros"),
+            body('avatar').notEmpty().withMessage("O preenchimento desse campo é obrigatório"),
+            body('avatar').isString().withMessage("Esse campo não aceita numeros"),
+            body('login').notEmpty().withMessage("O preenchimento desse campo é obrigatório"),
+            body('login').isString().withMessage("Esse campo não aceita numeros"),
+            body('password').notEmpty().withMessage("O preenchimento desse campo é obrigatório"),
+            body('password').isString().withMessage("Esse campo não aceita numeros"),
+            body('email').notEmpty().withMessage("O preenchimento desse campo é obrigatório"),
+            body('email').isString().withMessage("Esse campo não aceita numeros"),
+        ],
+
+        updating: async (req, res) => {
+
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+
+                return res.status(400).json({ errors: errors.array() });
+            } else {
+
+                const id = req.params.id
+                const {
+                    name,
+                    avatar,
+                    login,
+                    password,
+                    email
+                } = req.body;
+
+                try {
+
+                    await User.update(
+                        {
+                            name: name,
+                            avatar: avatar,
+                            login: login,
+                            password: password,
+                            email: email,
+                        },
+                        {
+                            where: { id: id }
+                        }
+                    )
+
+                    const user = await User.findByPk(id);
+                    if (!user) {
+                        throw new Error("Usuario não existe")
+                    }
+
+                    user.name = name;
+                    user.avatar = avatar;
+                    user.login = login;
+                    user.password = password;
+                    user.email = email;
+
+                    await user.save()
+
+                    res.send({ mensagem: "usuário atualizado com sucesso" })
+                } catch (error) {
+                    res.send({ error: error.message });
+                }
+
+            }
+        }
     }
-  },
 
-  async update(req, res) {
-    const { id } = req.params;
-    const {
-      name,
-      avatar,
-      login,
-      password,
-      email,
-    } = req.body;
 
-    try {
-      const user = await User.findByPk(id);
-      if (!user) {
-        throw new Error('Usuario não existe');
-      }
-
-      user.name = name;
-      user.avatar = avatar;
-      user.login = login;
-      user.password = password;
-      user.email = email;
-
-      await user.save();
-
-      res.send({ mensagem: 'usuário atualizado com sucesso' });
-    } catch (error) {
-      res.send({ error: error.message });
-    }
-  },
-
-};
+}
