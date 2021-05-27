79fa8ec7faee2a053c642a97ae57f486c830c8f2
Vitor Vaz <vitor.vaz.andrade@gmail.com>
2021-05-27 03:08:39 -0300

    ultima config teste para o banco

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -11,6 +11,8 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 
 (async () => {
   try {
+
+    await sequelize.sync();
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
   } catch (error) {
