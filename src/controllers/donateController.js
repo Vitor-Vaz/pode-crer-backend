1e78faf8bab2c603d62b2275db405e23c6d7165b
leo-tavares <leonardotavaresdev@gmail.com>
2021-05-30 22:23:44 -0300

    fix: calculo da donate

--- src/controllers/donateController.js
+++ src/controllers/donateController.js
@@ -1,96 +1,96 @@
 const { Op } = require('sequelize');
 const Donates = require('../models/donates');
 const Dream = require('../models/dream');
-const Users = require('../models/user')
+const Users = require('../models/user');
 const AppError = require('../helper/AppError');
 
 module.exports = {
+  async donating(req, res) {
+    const { userId, dreamId, value } = req.body;
+    try {
+      const user = await Users.findByPk(userId);
 
-    async donating(req, res) {
-        const { userId, dreamId, value } = req.body;
+      if (!user) {
+        throw new AppError({
+          message: 'Usuario não encontrado na base de dados',
+          statusCode: 404,
+        });
+      }
 
-        try {
-            const user = await Users.findByPk(userId);
+      if (value > user.coins) {
+        throw new AppError({
+          message:
+            'Não foi possivel fazer doação, valor de créditos insuficiente!',
+          statusCode: 203,
+        });
+      }
 
-            if(!user){
-                throw new AppError({ message: "Usuario não encontrado na base de dados", statusCode: 404 });
-            }
+      const dream = await Dream.findByPk(dreamId);
 
-            if (value > user.coins) {
-                throw new AppError({ message: "Não foi possivel fazer doação, valor de créditos insuficiente!", statusCode: 203 });
-            }
+      if (!dream) {
+        throw new AppError({
+          message: 'Sonho não encontrado na base de dados',
+          statusCode: 404,
+        });
+      }
 
-            const dream = await Dream.findByPk(dreamId);
+      user.coins -= Number(value);
+      await user.save();
 
-            if(!dream){
-                throw new AppError({ message: "Sonho não encontrado na base de dados", statusCode: 404 });
-            }
+      dream.reached += Number(value);
+      await dream.save();
 
+      const donate = await Donates.create({ userId, dreamId, value });
 
-            user.coins = (Number(user.coins) - value).toFixed(2);
-            await user.save();
-
-            dream.reached = (Number(dream.reached) + value).toFixed(2);
-            await dream.save();
-
-            const donate = await Donates.create({ userId, dreamId, value });
-
-            res.send(donate);
-
-
-
-        } catch (err) {
-            res.status(err.statusCode).send({ err: err.message });
-        }
-
-
-    },
-
-
-    async allDonatesInADream(req, res) {
-        const id = req.params.id;
-
-        try {
-            const donates = await Donates.findAll({
-                where: {
-                    dreamId: `${id}`
-                }
-            });
-
-            if (!donates.length) {
-                throw new AppError({ message: "Não foram encontrado registros de doações para esse sonho", statusCode: 404 });
-            }
-
-            res.send(donates)
-        } catch (error) {
-            res.status(error.statusCode).send({ error: error.message })
-        }
-
-    },
-
-    async allDonatesFromAUser(req, res) {
-
-        const id = req.params.id;
-
-        try {
-            const donates = await Donates.findAll({
-                where: {
-                    userId: `${id}`
-                }
-            });
-
-            if (!donates.length) {
-                throw new AppError({ message: "Não foram encontrado registros de doações para esse usuario", statusCode: 404 });
-            }
-            res.send(donates)
-
-        } catch (error) {
-            res.status(error.statusCode).send({ error: error.message })
-        }
-
-
-
-
+      res.send(donate);
+    } catch (err) {
+      res.status(err.statusCode).send({ err: err.message });
     }
+  },
 
-}
+  async allDonatesInADream(req, res) {
+    const { id } = req.params;
+
+    try {
+      const donates = await Donates.findAll({
+        where: {
+          dreamId: `${id}`,
+        },
+      });
+
+      if (!donates.length) {
+        throw new AppError({
+          message: 'Não foram encontrado registros de doações para esse sonho',
+          statusCode: 404,
+        });
+      }
+
+      res.send(donates);
+    } catch (error) {
+      res.status(error.statusCode).send({ error: error.message });
+    }
+  },
+
+  async allDonatesFromAUser(req, res) {
+    const { id } = req.params;
+
+    try {
+      const donates = await Donates.findAll({
+        where: {
+          userId: `${id}`,
+        },
+      });
+
+      if (!donates.length) {
+        throw new AppError({
+          message:
+            'Não foram encontrado registros de doações para esse usuario',
+          statusCode: 404,
+        });
+      }
+      res.send(donates);
+    } catch (error) {
+      res.status(error.statusCode).send({ error: error.message });
+    }
+  },
+};
