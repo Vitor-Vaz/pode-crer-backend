8bae94fa62ca8fdf2ffa7d13bb17f1dda99e6397
leo-tavares <leonardotavaresdev@gmail.com>
2021-06-04 15:09:13 -0300

    fix: critical issue config database

--- src/config/db/database.js
+++ src/config/db/database.js
@@ -1,27 +1,16 @@
 module.exports = {
   development: {
-    host: 'ec2-54-224-194-214.compute-1.amazonaws.com',
-    username: 'oujpmkqkyxsuxt',
-    password: 'ff396a2538998bdb6f0c78b6aff58a1eaf0d2e3c48a9c265aaff9c7525d8837d',
-    storage: process.env.DATABASE_URL,
+    databse: 'podecrerdb',
+    host: '127.0.0.1',
     dialect: 'postgres',
   },
   test: {
-    username: process.env.CI_DB_USERNAME,
-    password: process.env.CI_DB_PASSWORD,
-    database: process.env.CI_DB_NAME,
+    database: 'podecrerdb',
     host: '127.0.0.1',
-    port: 3306,
-    dialect: 'sqlite',
-    dialectOptions: {
-      bigNumberStrings: true,
-    },
+    dialect: 'postgres',
   },
   production: {
-    host: 'ec2-54-224-194-214.compute-1.amazonaws.com',
-    username: 'oujpmkqkyxsuxt',
-    password: 'ff396a2538998bdb6f0c78b6aff58a1eaf0d2e3c48a9c265aaff9c7525d8837d',
-    storage: process.env.DATABASE_URL,
+    use_env_variable: process.env.DATABASE_URL,
     dialect: 'postgres',
   },
 };
