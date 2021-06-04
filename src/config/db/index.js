395008f65cd10ae2a32e096099cad647594e1b1c
leo-tavares <leonardotavaresdev@gmail.com>
2021-06-04 14:38:33 -0300

    chore: Documentação Swagger das rotas #23

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,17 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions:{
+const sequelize = new Sequelize('postgres://lggfxzkhmaaehf:486e0b872588992d8d9e9018fe63fdaedff3bd58b6319f775ea317dae24e0f60@ec2-52-205-3-3.compute-1.amazonaws.com:5432/d3ii8goos7kube', {
+  dialectOptions: {
     ssl: {
       rejectUnauthorized: false,
-    }
-  }
+    },
+  },
 }); // Example for sqlite
-
 
 (async () => {
   try {
-
     await sequelize.sync();
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
--- src/server.js
+++ src/server.js
@@ -3,7 +3,9 @@ require('express-async-errors');
 require('dotenv').config();
 require('./config/firebase');
 require('./config/db');
+const swaggerUi = require('swagger-ui-express');
 const cors = require('cors');
+const swaggerFile = require('../swagger_output.json');
 const globalError = require('./middlewares/globalError');
 
 const port = process.env.PORT || 3000;
@@ -14,6 +16,7 @@ const routes = require('./routes');
 server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
+server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 server.use(routes);
 server.use(globalError);
 
